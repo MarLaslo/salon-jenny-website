63315a473f6d907c7fd36ed152dede589821e5b0
MarLaslo <marek.laslo@icloud.com>
2024-12-20 21:56:46 +0100

    setting router

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+export const HomePage = () => {
+  return (
+    <div>
+      <h1>Salón pre psov Jenny</h1>
+      <p>
+        Vitajte na stránke salónu pre psov Jenny. Naša ponuka je zameraná na
+        kvalitné strihanie psov a iné služby spojené s údržbou psov.
+      </p>
+      <p>V prípade záujmu nás neváhajte kontaktovať na telefónnom čísle </p>
+    </div>
+  );
+};
