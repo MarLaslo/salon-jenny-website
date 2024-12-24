78bd12f296cb949471a398213e752982ad32373e
MarLaslo <marek.laslo@icloud.com>
2024-12-24 18:02:39 +0100

    navbar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,48 +1,87 @@
+import { useState } from 'react';
+
 export const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const buttons = [
     ['Domov', '/'],
     ['Cenník', '/'],
-    ['Služby', '/'],
     ['O mne', '/'],
+    ['Pred návštevou', '/'],
     ['Kontakt', '/'],
+    ['Ozónoterapia', '/'],
   ];
 
+  const handleClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log(isMenuOpen);
+  };
+
   return (
-    <nav className="fixed start-0 top-0 z-20 w-full bg-blue-400">
-      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img
-            src="src/assets/0.png"
-            alt="Salon pre psov Jenny"
-            className="m-0 w-10 p-0"
-          />
-          <span className="hidden self-center whitespace-nowrap text-2xl font-bold text-cyan-50 lg:order-1 lg:flex lg:w-auto">
-            Salón pre psov Jenny
-          </span>
-        </a>
-        <div className="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
+    <>
+      <nav className="sticky start-0 top-0 z-20 w-full bg-blue-400">
+        <div className="flex flex-wrap items-center justify-center lg:justify-between">
+          <a href="/" className="ml-4 flex items-start space-x-3 py-2">
+            <img
+              src="src/assets/0.png"
+              alt="Salón pre psov Jenny"
+              className="m-0 w-10 p-0"
+            />
+            <span
+              className="self-center whitespace-nowrap text-2xl font-bold text-cyan-50 lg:flex lg:w-auto"
+              alt="Salón Jenny"
+            >
+              Salón Jenny
+            </span>
+          </a>
+          <div className="ml-auto hidden lg:flex">
+            {buttons.map(([title, url]) => (
+              <div
+                className="font-extrabold text-white hover:bg-blue-300 hover:text-blue-500"
+                key={title}
+              >
+                <a href={url} className="block px-4 py-4">
+                  {title}
+                </a>
+              </div>
+            ))}
+          </div>
           <button
             type="button"
-            className="hidden rounded-md bg-blue-500 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-300 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:order-1 sm:flex sm:w-auto"
+            className="ml-8 lg:ml-auto mr-4 hidden rounded-md bg-blue-600 px-4 py-2 text-center text-sm font-medium text-white transition duration-300 ease-in-out hover:bg-blue-700 min-[400px]:flex min-[400px]:w-auto"
           >
             Objednať sa
           </button>
-        </div>
-        <div
-          className="flex w-auto items-center justify-between px-6 lg:px-3"
-          id="navbar-sticky"
-        >
-          {buttons.map(([title, url]) => (
-            <a
-              key={title}
-              href={url}
-              className="rounded-lg px-1 py-0 font-medium text-white hover:bg-blue-300 hover:text-blue-500 sm:px-3 sm:py-3"
+          <button className="mr-4 lg:hidden" onClick={() => handleClick()}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8 stroke-current stroke-2 text-white transition duration-300 ease-in-out hover:text-blue-500"
+              fill="none"
+              viewBox="0 0 24 24"
             >
-              {title}
-            </a>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </nav>
+      {isMenuOpen && (
+        <div className="lg:hidden sticky start-0 top-0 z-20 w-full drop-shadow-2xl">
+          {buttons.map(([title, url]) => (
+            <div
+              className="h-11 bg-blue-400 py-1 text-center text-sm font-bold text-white transition duration-300 ease-out hover:bg-blue-300 hover:text-blue-500"
+              key={title}
+            >
+              <a href={url} className="block px-4 py-2">
+                {title}
+              </a>
+            </div>
           ))}
         </div>
-      </div>
-    </nav>
+      )}
+    </>
   );
 };
