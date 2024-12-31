75166470da19b670cc9f5887a9b44869d6c808e7
MarLaslo <marek.laslo@icloud.com>
2024-12-31 15:53:29 +0100

    contact and pricelist changes

--- src/components/PriceList.jsx
+++ src/components/PriceList.jsx
@@ -19,6 +19,12 @@ export const PriceList = () => {
                     })}
                 </div>
             </div>
+            <div className="container mx-auto mb-2 rounded-3xl border-b-2 border-t-2 border-blue-200/60 bg-blue-300/60 p-4 px-8 shadow-lg">
+                <p>
+                    <strong className="text-blue-500">V cene každej služby: </strong>
+                    Čistenie uší, očí a strihanie pazúrov.
+                </p>
+            </div>
             <div className="container mx-auto mb-20 rounded-3xl border-b-2 border-t-2 border-red-200/60 bg-red-300/60 p-4 px-8 shadow-lg">
                 <p>
                     <strong className="text-red-500">Dôležité 1: </strong>
